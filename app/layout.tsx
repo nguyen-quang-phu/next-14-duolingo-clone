56e254913ae9b8ebc647512f888b544bcc7dccf5
Harvey <nqphu1998@gmail.com>
2024-03-22 15:57:47 +0700

    feat: add eslint rule

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+const hello = "world"
 
 export const metadata: Metadata = {
   title: "Create Next App",
